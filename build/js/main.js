a7492441e0bf927f1da100355d31b8418805194f
Alberto Ribeiro <202268457+ARibeiroDev@users.noreply.github.com>
2025-07-15 14:50:22 +0200

    first commit

---
+++ build/js/main.js
@@ -0,0 +1,18 @@
+const initApp = () => {
+  const hamburgerBtn = document.getElementById("hamburger-button");
+  const mobileMenu = document.getElementById("mobile-menu");
+
+  const toggleMenu = () => {
+    mobileMenu.classList.toggle("hidden");
+    mobileMenu.classList.toggle("flex");
+    hamburgerBtn.classList.toggle("toggle-btn");
+  };
+
+  hamburgerBtn.addEventListener("click", toggleMenu);
+  mobileMenu.addEventListener("click", toggleMenu);
+
+  const year = document.getElementById("year");
+  year.textContent = new Date().getFullYear();
+};
+
+document.addEventListener("DOMContentLoaded", initApp);
